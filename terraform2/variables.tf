f3977c014cea5c1ec15b39a6d03bee61fc2698e8
amrit <amritpoudel433@gmail.com>
2025-07-13 00:08:32 +0545

    feat!: created the s3 bucket

--- terraform2/variables.tf
+++ terraform2/variables.tf
@@ -1,23 +1,23 @@
 variable "region" {
-    description = "region for the aws"
-    type = string
-  
+  description = "region for the aws"
+  type        = string
+
 }
 
 
 
 variable "account_id" {
-    description = "the account id for creating the bucket policy"
-    type = number
-  
+  description = "the account id for creating the bucket policy"
+  type        = number
+
 }
 variable "should_enable_versioning" {
-    description = "The versioning of the bucket"  
-    type = bool
-    default = false
+  description = "The versioning of the bucket"
+  type        = bool
+  default     = false
 }
 # variable "bucket_name" {
 #     description = "the name of the bucket"
 #     type = string
-  
+
 # }
