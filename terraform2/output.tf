0bfa0c9664e583e1ab4bf0f43da98aaaeac38035
amrit <amritpoudel433@gmail.com>
2025-07-15 00:43:32 +0545

    feat(terraform2): fixed the s3 bucket name varibale and output

--- terraform2/output.tf
+++ terraform2/output.tf
@@ -1,7 +1,8 @@
 output "bucket_id" {
-  value = module.s3_bucket.bucket_id
+  value = module.ec2_s3_bucket.bucket_id
+  
 }
 
 output "bucket_arn" {
-  value = module.s3_bucket.bucket_arn
+  value = module.ec2_s3_bucket.bucket_arn
 }
