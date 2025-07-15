ca55ecd6a4f17aaf31c4d2216b4319513f698892
amrit <amritpoudel433@gmail.com>
2025-07-15 07:22:53 +0545

    fix(terraform2): fixed the terraform2 value

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -71,7 +71,7 @@ resource "aws_s3_bucket_policy" "policy" {
     Statement = [{
       Effect = "Allow",
       Principal = {
-        AWS = "arn:aws:iam::${var.account_id}:role/ec2_access_s3_role_${var.env}"
+        AWS = "arn:aws:iam::${var.account_id}:role/ec2_role_arn${var.env}"
       }
       Action   = ["s3:GetObject", "s3:PutObject"],
       Resource = "${module.ec2_s3_bucket.bucket_arn}/*"
